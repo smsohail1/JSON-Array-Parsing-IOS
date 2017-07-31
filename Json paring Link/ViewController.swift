5fb0f5b54591bfaba1e978aac8bd987d1be0ae30
Abdul Ahad <abdulahad@Abduls-iMac.local>
2017-07-31 01:00:07 -0700

    Version 1.0

--- Json paring Link/ViewController.swift
+++ Json paring Link/ViewController.swift
@@ -12,7 +12,83 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        let config = URLSessionConfiguration.default // Session Configuration
+        let session = URLSession(configuration: config) // Load into Session
+        let url = URL(string: "https://api.randomuser.me")!
+        
+        let task = session.dataTask(with: url, completionHandler: {
+            (data, response, error) in
+            
+            if error != nil {
+                
+                print(error!.localizedDescription)
+                
+            } else {
+                
+                do {
+                    
+                    if let json = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
+                    {
+                        
+                        //Implement your logic
+                        //print(json)
+                        
+                        
+                        let result = json["results"] as! [[String:Any]]
+                        print("\(result)")
+                        
+                        let info = json["info"] as! [String:AnyObject]
+                        
+                     let gender =   result.first?["gender"] as! String
+                        print("\(gender)")
+                        
+                        
+                        
+                     //   let gender = result["gender"] as! [[String:Any]]
+                      //  print("\(gender)")
+                        
+                        let name =  result.first?["name"] as! [String:AnyObject]
+                        
+                        for item in name
+                        {
+                            print("\(item.key),,,,\(item.value)")
+                        }
+                        
+                        let email = result.first?["email"] as! String
+                        print("\(email)")
+                        
+                        
+                        let login = result.first?["login"] as! [String:AnyObject]
+                        
+                        for item in login{
+                            print("\(item.key),,\(item.value)")
+                        }
+                        
+                        print("\(info)")
+                       
+                        for item in info{
+                            print("\(item.key) ,,, \(item.value)")
+                        }
+                        
+                    }
+                    
+                }
+                catch {
+                    
+                    print("error in JSONSerialization")
+                    
+                }
+                
+                
+            }
+            
+        })
+        task.resume()
+        
+        
+        
+        
     }
 
    
